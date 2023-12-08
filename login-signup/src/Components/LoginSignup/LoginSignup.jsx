138c6c8b70248240fd9954663e7d62812999db26
say00j <sayoojsayana123@gmail.com>
2023-12-08 21:22:45 +0530

    Update LoginSignup.jsx

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './LoginSignup.css'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='container'>
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+       <div className="input">
+        <img src={user_icon} alt="" />
+        <input type="text" />
+       </div>
+      </div>
+      
+       <div className="input">
+        <img src={email_icon} alt="" />
+        <input type="email" />
+       </div>
+     
+     
+       <div className="input">
+        <img src={password_icon} alt="" />
+        <input type="password" />
+       </div>
+       <div className="submit-container">
+        <div className="submit">Sign Up</div>
+       </div>
+    
+      </div>
   )
 }
