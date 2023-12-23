95a704d82eea827813c34b49dffd6c2d34676f82
say00j <sayoojsayana123@gmail.com>
2023-12-23 20:25:29 +0530

    update

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { LoginSignup } from './Components/LoginSignup/LoginSignup';
 
+
 function App() {
   return (
+   
     <div>
       <LoginSignup/>
     </div>
+    
   );
 }
 
