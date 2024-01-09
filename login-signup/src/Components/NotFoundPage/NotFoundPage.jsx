5431faf3d3a5f9e16479734809867f1fcb4249e9
say00j <sayoojsayana123@gmail.com>
2024-01-09 17:09:14 +0530

    Update NotFoundPage.jsx

--- login-signup/src/Components/NotFoundPage/NotFoundPage.jsx
+++ login-signup/src/Components/NotFoundPage/NotFoundPage.jsx
@@ -11,15 +11,4 @@ export const NotFoundPage = () => {
   );
 };
 
-// NotFoundPage component
-import React from 'react';
-
-export const NotFoundPage = () => {
-  return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-    </div>
-  );
-};
 
