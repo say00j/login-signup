1572e1ca035e49befb49285215316ec9520707fc
say00j <sayoojsayana123@gmail.com>
2024-01-11 10:15:32 +0530

    update

--- login-signup/src/Components/NotFoundPage/NotFoundPage.jsx
+++ login-signup/src/Components/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,10 @@
-
+import './NotFoundPage.css' 
 // NotFoundPage component
 import React from 'react';
 
 export const NotFoundPage = () => {
   return (
-    <div>
+    <div className='background'>
       <h1>404 - Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
